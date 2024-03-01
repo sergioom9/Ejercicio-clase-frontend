4defb3f012481673b7422ac9c38914819561237d
sergioom9 <145363747+sergioom9@users.noreply.github.com>
2024-03-01 14:12:06 +0100

    Add files via upload

--- Ejercicio-Clase/components/AddPost.tsx
+++ Ejercicio-Clase/components/AddPost.tsx
@@ -5,8 +5,11 @@ const AddPost: FunctionComponent = () => {
     <form class="AddForm" action="/add" method="post">
       <h1>Add new Post</h1>
       <input type="text" titulo="titulo" placeholder="Titulo" />
+      <div></div>
       <input type="text" autor="autor" placeholder="Autor" />
+      <div></div>
       <input type="text" contenido="contenido" placeholder="Descripcion" />
+      <div></div>
       <button type="submit">Add</button>
     </form>
   );
